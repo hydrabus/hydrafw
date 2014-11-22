d36d4f93d7cf017a9720ed883a53111fc89640c3
Bert Vermeulen <bert@biot.com>
2014-11-22 22:42:50 +0100

    Add copyright messages.

--- common/common.c
+++ common/common.c
@@ -1,5 +1,6 @@
 /*
-HydraBus/HydraNFC - Copyright (C) 2014 Benjamin VERNOUX
+	HydraBus/HydraNFC - Copyright (C) 2014 Benjamin VERNOUX
+	Copyright (C) 2014 Bert Vermeulen <bert@biot.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
