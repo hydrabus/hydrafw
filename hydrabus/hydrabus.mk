21acb6d2372d525e0d27b1d35729f49322ff6470
Bert Vermeulen <bert@biot.com>
2014-12-01 17:11:25 +0100

    Remove HiZ mode.
    This did nothing.

--- hydrabus/hydrabus.mk
+++ hydrabus/hydrabus.mk
@@ -3,7 +3,6 @@ HYDRABUSSRC = hydrabus/hydrabus.c \
             hydrabus/commands.c \
 			hydrabus/hydrabus_adc.c \
             hydrabus/hydrabus_mode.c \
-            hydrabus/hydrabus_mode_hiz.c \
             hydrabus/hydrabus_mode_spi.c \
             hydrabus/hydrabus_mode_uart.c \
             hydrabus/hydrabus_mode_i2c.c
