19b1e927a5d418f246d0f5660e7bf4c239cda9ba
bvernoux <bvernoux@gmail.com>
2015-01-12 23:02:55 +0100

    Fixed hydrafw-version.py HYDRAFW_GIT_TAG to have always hash (long mode)

--- scripts/hydrafw-version.py
+++ scripts/hydrafw-version.py
@@ -12,7 +12,7 @@ parser = OptionParser(usage=usage)
 if len(args)==1:
   sys.stdout = open(args[0], 'w')
   git=Repo().git
-  print '#define HYDRAFW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True) + '"'
+  print '#define HYDRAFW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"'
   print '#define HYDRAFW_BUILD_DATE "' + date.today().isoformat() + '"'
 else:
   parser.print_help()
