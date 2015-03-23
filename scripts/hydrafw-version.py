ff70ffb08db947f1166d3f415b5634b817a77ee8
Benjamin Vernoux <bvernoux@gmail.com>
2015-03-23 22:56:08 +0100

    Modification to GitPython has broken the build
    Becareful this modification requires you use latest updated GitPython "python -m pip install GitPython --upgrade" see wiki for more details.

--- scripts/hydrafw-version.py
+++ scripts/hydrafw-version.py
@@ -11,7 +11,7 @@ parser = OptionParser(usage=usage)
 (options, args) = parser.parse_args()
 if len(args)==1:
   sys.stdout = open(args[0], 'w')
-  git=Repo().git
+  git=Repo(search_parent_directories=True).git
   print '#define HYDRAFW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"'
   print '#define HYDRAFW_BUILD_DATE "' + date.today().isoformat() + '"'
 else:
