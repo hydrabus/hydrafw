b3b25da016eeddd12553e83cad044d7eb27501e2
Baldanos <balda@balda.ch>
2015-11-29 16:53:07 +0100

    Typo in my name

--- drv/stm32cube/bsp_can_conf.h
+++ drv/stm32cube/bsp_can_conf.h
@@ -1,5 +1,5 @@
 /*
-HydraBus/HydraNFC - Copyright (C) 2014-2015 Benjamin VERNOUX
+HydraBus/HydraNFC - Copyright (C) 2015 Nicolas OBERLI
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- hydrabus/hydrabus_sump.h
+++ hydrabus/hydrabus_sump.h
@@ -1,7 +1,7 @@
 /*
  * HydraBus/HydraNFC
  *
- * Copyright (C) 2015 Nioclas OBERLI
+ * Copyright (C) 2015 Nicolas OBERLI
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
