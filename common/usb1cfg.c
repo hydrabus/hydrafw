ff2d37d2d0a8b0155c01eb5bbece58501e8355d8
bvernoux <bvernoux@gmail.com>
2016-06-20 12:56:17 +0200

    Add event USB_EVENT_UNCONFIGURED in usb1cfg.c & usb2cfg.c

--- common/usb1cfg.c
+++ common/usb1cfg.c
@@ -339,6 +339,8 @@ static void usb_event(USBDriver *usbp, usbevent_t event)
 
 		chSysUnlockFromISR();
 		return;
+	case USB_EVENT_UNCONFIGURED:
+		return;
 	case USB_EVENT_SUSPEND:
 		chSysLockFromISR();
 
