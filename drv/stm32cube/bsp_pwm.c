eca1bffa1c7e07570235c2c8774adb1806328725
bvernoux <bvernoux@gmail.com>
2016-09-27 19:56:57 +0200

    bsp_pwm_update() fix coverity scan bug "uninitialized value htim.Instance"

--- drv/stm32cube/bsp_pwm.c
+++ drv/stm32cube/bsp_pwm.c
@@ -206,13 +206,14 @@ bsp_status_t bsp_pwm_update(bsp_dev_pwm_t dev_num, uint32_t frequency, uint32_t 
 
 	channel = get_pwm_chan_num(dev_num);
 
+	/* Configure TIMx PWM on Timer2 */
+	htim.Instance = TIM2;
+
 	/* Stop PWM */
 	if(HAL_TIM_PWM_Stop(&htim, channel) != HAL_OK) {
 		return BSP_ERROR;
 	}
 
-	/* Configure TIMx PWM on Timer2 */
-	htim.Instance = TIM2;
 	/* Configure PWM Frequency */
 	arr = ((apb1_freq * 2) / frequency) - 1;
 	htim.Instance->ARR = arr;
