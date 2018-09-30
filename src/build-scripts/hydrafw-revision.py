18aaa6d8f0415d19bdb08bfbf70aec7f08a43de3
Baldanos <balda@balda.ch>
2018-09-30 14:20:44 +0200

    Add firmware revision info in DFU file (#84)

---
+++ src/build-scripts/hydrafw-revision.py
@@ -0,0 +1,8 @@
+from git import *
+import re
+
+r = re.compile("v(\d+\.\d+).*")
+
+git=Repo(search_parent_directories=True).git
+version = git.describe(tags=True,always=True,dirty=True,long=True)
+print(r.search(version).group(1))
