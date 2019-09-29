bf3af42f3349586b66258a81f194aac648b312a3
ghecko <ovre.jordan@gmail.com>
2019-09-29 15:19:11 +0200

    Add in_waiting @property

--- contrib/pyHydrabus/pyHydrabus/hydrabus.py
+++ contrib/pyHydrabus/pyHydrabus/hydrabus.py
@@ -84,6 +84,15 @@ class Hydrabus:
         """
         self._serialport.reset_input_buffer()
 
+    @property
+    def in_waiting(self):
+        """
+        Return the number of bytes in the receive buffer.
+        :return: Number of bytes
+        :rtype: int
+        """
+        return self._serialport.in_waiting
+
     def exit_bbio(self):
         """
         Reset Hydrabus to CLI mode
