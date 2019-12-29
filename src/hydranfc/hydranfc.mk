f4fce22569f66823440d68b77cca233f292126c4
bvernoux <bvernoux@gmail.com>
2019-12-29 13:20:34 +0100

    Cleanup hydranfc.mk

--- src/hydranfc/hydranfc.mk
+++ src/hydranfc/hydranfc.mk
@@ -3,11 +3,10 @@ HYDRANFCSRC = hydranfc/hydranfc.c \
               hydranfc/hydranfc_cmd_sniff.c \
               hydranfc/hydranfc_cmd_sniff_downsampling.c \
               hydranfc/hydranfc_cmd_sniff_iso14443.c \
-              hydranfc_emul_14443a_sdd.c \
-              hydranfc_emul_mifare.c \
-              file_fmt_pcap.c \
-              hydranfc_emul_mf_ultralight.c
+              hydranfc/hydranfc_emul_14443a_sdd.c \
+              hydranfc/hydranfc_emul_mifare.c \
+              hydranfc/file_fmt_pcap.c \
+              hydranfc/hydranfc_emul_mf_ultralight.c
 
 # Required include directories
-HYDRANFCINC = ./hydranfc \
-              ./hydranfc/low_level
+HYDRANFCINC = ./hydranfc
