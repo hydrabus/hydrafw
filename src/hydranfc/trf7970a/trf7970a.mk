56fa5bb12be595e92e8e1fb0c58cc5664b856c2c
bvernoux <bvernoux@gmail.com>
2019-12-31 03:02:50 +0100

    Moved trf7970a directorty to hydranfc/trf7970a

---
+++ src/hydranfc/trf7970a/trf7970a.mk
@@ -0,0 +1,8 @@
+# List of all the HydraNFC TRF7970A files, there is no need to remove the files
+TRFSRC = ./hydranfc/trf7970a/src/mcu.c \
+         ./hydranfc/trf7970a/src/trf797x.c \
+         ./hydranfc/trf7970a/src/trf_spi.c \
+         ./hydranfc/trf7970a/src/tools.c
+
+# Required include directories
+TRFINC = ./hydranfc/trf7970a/include
--- src/trf7970a/trf7970a.mk
+++
@@ -1,8 +0,0 @@
-# List of all the HydraNFC TRF7970A files, there is no need to remove the files
-TRFSRC = ./trf7970a/src/mcu.c \
-         ./trf7970a/src/trf797x.c \
-         ./trf7970a/src/trf_spi.c \
-         ./trf7970a/src/tools.c
-
-# Required include directories
-TRFINC = ./trf7970a/include
