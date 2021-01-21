b81bd088b725ce4ad5f6a3141834001d2f66b246
Baldanos <balda@balda.ch>
2021-01-21 19:34:04 +0100

    Remove use of `g_sbuf` in BBIO smartcard

--- src/hydrabus/hydrabus_bbio_smartcard.c
+++ src/hydrabus/hydrabus_bbio_smartcard.c
@@ -55,13 +55,19 @@ void bbio_mode_smartcard(t_hydra_console *con)
 {
 	uint32_t to_rx, to_tx, i;
 	uint8_t bbio_subcommand;
-	uint8_t *tx_data = (uint8_t *)g_sbuf;
-	uint8_t *rx_data = (uint8_t *)g_sbuf+4096;
+	uint8_t *tx_data = pool_alloc_bytes(0x1000); // 4096 bytes
+	uint8_t *rx_data = pool_alloc_bytes(0x1000); // 4096 bytes
 	uint8_t data;
 	uint32_t dev_speed=0;
 	uint32_t final_baudrate;
 	bsp_status_t status;
 	mode_config_proto_t* proto = &con->mode->proto;
+
+	if(tx_data == 0 || rx_data == 0) {
+		pool_free(tx_data);
+		pool_free(rx_data);
+		return;
+	}
 
 	bbio_smartcard_init_proto_default(con);
 	bsp_smartcard_init(proto->dev_num, proto);
@@ -72,6 +78,8 @@ void bbio_mode_smartcard(t_hydra_console *con)
 		if(chnRead(con->sdu, &bbio_subcommand, 1) == 1) {
 			switch(bbio_subcommand) {
 			case BBIO_RESET:
+				pool_free(tx_data);
+				pool_free(rx_data);
 				bsp_smartcard_deinit(proto->dev_num);
 				return;
 			case BBIO_MODE_ID:
@@ -202,4 +210,7 @@ void bbio_mode_smartcard(t_hydra_console *con)
 			}
 		}
 	}
+	pool_free(tx_data);
+	pool_free(rx_data);
+	bsp_smartcard_deinit(proto->dev_num);
 }
