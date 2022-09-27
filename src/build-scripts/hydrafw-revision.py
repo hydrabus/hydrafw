f09a5e04c72243b073621f066fe65605360fef18
Jan-Benedict Glaw <jbglaw@lug-owl.de>
2022-09-27 21:56:40 +0200

    Fix 2to3 oddities

--- src/build-scripts/hydrafw-revision.py
+++ src/build-scripts/hydrafw-revision.py
@@ -7,4 +7,4 @@ r = re.compile("v(\d+\.\d+).*")
 
 git=Repo(search_parent_directories=True).git
 version = git.describe(tags=True,always=True,dirty=True,long=True)
-print((r.search(version).group(1)))
+print(r.search(version).group(1))
