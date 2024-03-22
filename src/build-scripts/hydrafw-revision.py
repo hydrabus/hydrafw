0c85199e32a87ec0a96a12aef9960db058b75ef3
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-22 12:38:30 +0300

    workflow/push.yml - debugging hfw-rev.py on GH build server

--- src/build-scripts/hydrafw-revision.py
+++ src/build-scripts/hydrafw-revision.py
@@ -7,4 +7,6 @@ r = re.compile("v(\d+\.\d+).*")
 
 git=Repo(search_parent_directories=True).git
 version = git.describe(tags=True,always=True,dirty=True,long=True)
+print("VERSION:", version)
+print("SEARCH:", r.search(version))
 print(r.search(version).group(1))
