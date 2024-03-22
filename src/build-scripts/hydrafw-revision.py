a8de2879b770fe24fab715ba2b426b34678f083e
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-22 13:28:31 +0300

    hydrafw-revision.py: add exception

--- src/build-scripts/hydrafw-revision.py
+++ src/build-scripts/hydrafw-revision.py
@@ -5,9 +5,12 @@ import re
 
 r = re.compile("v(\d+\.\d+).*")
 
-git=Repo(search_parent_directories=True).git
-version = git.describe(tags=True,always=True,dirty=True,long=True)
-if r.search(version):
-    print(r.search(version).group(1))
-else:
+try:
+    git=Repo(search_parent_directories=True).git
+    version = git.describe(tags=True,always=True,dirty=True,long=True)
+    if r.search(version):
+        print(r.search(version).group(1))
+    else:
+        print("0.0")
+except git.exc.InvalidGitRepositoryError:
     print("0.0")
