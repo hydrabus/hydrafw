6159c5892b3aa1c0975774fd3cafe2d4ecb7c89f
ia <ivan.a.zorin@gmail.com>
2024-03-27 13:00:47 +0300

    scripts/env.sh - add a couple of comments with clarifications

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# This script is used to enroll toolchain with GitHub Actions for CI/CD purposes
+# But this script can be used as reference to install recommended toolchain for building hydrafw.dfu locally as well
+
 MD5SUM=8a4a74872830f80c788c944877d3ad8c
 TARBALL=gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
 TARDIR=gcc-arm-none-eabi-4_9-2015q3
