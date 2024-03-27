46cde8e7c4e2de86f6c166972c871243fe28a57e
ia <ivan.a.zorin@gmail.com>
2024-03-27 12:10:52 +0300

    scripts/env.sh - add script to enroll reference toolchain

---
+++ scripts/env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+MD5SUM=8a4a74872830f80c788c944877d3ad8c
+TARBALL=gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
+TARDIR=gcc-arm-none-eabi-4_9-2015q3
+LINK=https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/"${TARBALL}"
+CURDIR="$(pwd)"
+
+echo  "${MD5SUM} ${TARBALL}"  >  md5.txt
+wget  "${LINK}"
+md5sum  -c  md5.txt
+
+tar  xvjf  "${TARBALL}"
+
+echo  "export  PATH=\"${CURDIR}/${TARDIR}/bin\":\"\${PATH}\"" > build.env
+
