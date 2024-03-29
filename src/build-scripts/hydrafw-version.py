116ec7659ac5ab4c4774116f98f8ebed7c3f45c2
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-03-29 18:18:50 +0300

    hydrafw-version.py - taking into consideration original sha id before detaching on github

--- src/build-scripts/hydrafw-version.py
+++ src/build-scripts/hydrafw-version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 from optparse import OptionParser
 from datetime import *
 from git import *
@@ -14,7 +15,12 @@ parser = OptionParser(usage=usage)
 if len(args)==1:
   sys.stdout = open(args[0], 'w')
   git=Repo(search_parent_directories=True).git
-  print('#define HYDRAFW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"')
+  vermagic=git.describe(tags=True,always=True,dirty=True,long=True)
+  if os.environ.get("GITHUB_CI_PR_SHA", "") != "":
+    sha_id=os.environ["GITHUB_CI_PR_SHA"][:7]
+    print('#define HYDRAFW_GIT_TAG "' + re.sub('-g.*$', '-g' + sha_id, vermagic) + '"')
+  else:
+    print('#define HYDRAFW_GIT_TAG "' + vermagic + '"')
   print('#define HYDRAFW_CHECKIN_DATE "' + git.show(['-s', '--pretty=format:%ai']).partition(' ')[0] + '"')
 else:
   parser.print_help()
