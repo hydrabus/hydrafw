4390579fc0b76c91cc8c5eee18c92504404a6b22
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-04-02 14:14:04 +0300

    scripts/env.sh: remove printf-like debugging for git meta info

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,44 +1,5 @@
 #!/usr/bin/env bash
-set -e
-
-set +e
-set -x
-
-git describe --tags  HEAD^1
-git describe --tags
-
-echo "-------------------------"
-
-echo "${GITHUB_CI_PR_SHA}"
-sha="`git  rev-parse  --short=8  HEAD`"
-tag="`git  tag  --points-at  ${sha}`"
-git  rev-parse  --symbolic-full-name  --short  HEAD
-date +%Y-%m-%d
-git  log  --pretty=oneline  v0.11..master
-git  log  --pretty=oneline  v0.11..master
-git  log  --pretty=oneline  v0.11...master
-git  log  --pretty=oneline  v0.11...master
-
-git branch -a
-
-fw_tag="`git describe --tags --abbrev=0`"
-fw_id="`git  rev-list  -n 1  ${fw_tag}`"
-
-git  log  --pretty=oneline  ${fw_id}..origin/master
-git  log  --pretty=oneline  ${fw_id}..origin/master | wc -l
-NUMERB22=git  log  --pretty=oneline  ${fw_id}..origin/master | wc -l
-
-echo "====="
-echo "${NUMBER22}"
-echo "====="
-
-git  log  --pretty=oneline  d6d65865716adc1419cc89e59d05f99dc99f89a9..origin/master
-git  log  --pretty=oneline  v0.11..origin/master
-git  log  --pretty=oneline  d6d65865716adc1419cc89e59d05f99dc99f89a9..remotes/origin/master
-git  log  --pretty=oneline  d6d65865716adc1419cc89e59d05f99dc99f89a9..hydrabus/master
-
-echo `git describe --tags --abbrev=0`-`git  log  --pretty=oneline  v0.11..master  |  wc -l`
-
+#set -x
 set -e
 
 # This script is used to enroll toolchain with GitHub Actions for CI/CD purposes
