11c05b345e1eca21f92b796c986a039323e933c9
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-04-02 14:11:18 +0300

    hydrafw-version.py: add brief comments not to forget whats going on here

--- src/build-scripts/hydrafw-version.py
+++ src/build-scripts/hydrafw-version.py
@@ -18,9 +18,11 @@ if len(args)==1:
   git=Repo(search_parent_directories=True).git
   vermagic=git.describe(tags=True,always=True,dirty=True,long=True)
   if os.environ.get("GITHUB_CI_PR_SHA", "") != "":
+    # if GITHUB_CI_PR_SHA is set, then it's GitHub Actions build, so workaround to set a valid tag is required
     sha_id=os.environ["GITHUB_CI_PR_SHA"][:7]
     print('#define HYDRAFW_GIT_TAG "' + re.sub('-g.*$', '-g' + sha_id, vermagic) + '"')
   else:
+    # If it's local build, then set "normal" tag
     print('#define HYDRAFW_GIT_TAG "' + vermagic + '"')
   print('#define HYDRAFW_CHECKIN_DATE "' + git.show(['-s', '--pretty=format:%ai']).partition(' ')[0] + '"')
 else:
