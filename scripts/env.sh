ecfb2cc379999b738a93dd801501af16bda2600a
Ivan Zorin <ivan.a.zorin@gmail.com>
2024-04-08 19:03:54 +0300

    CI/CD basic implementation based on GitHub Actions (#167)
    CI/CD basic implementation based on GitHub Actions

---
+++ scripts/env.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#set -x
+set -e
+
+# This script is used to enroll toolchain with GitHub Actions for CI/CD purposes
+# But this script can be used as reference to install recommended toolchain for building hydrafw.dfu locally as well
+
+MD5SUM=8a4a74872830f80c788c944877d3ad8c
+TARBALL=gcc-arm-none-eabi-4_9-2015q3-20150921-linux.tar.bz2
+TARDIR=gcc-arm-none-eabi-4_9-2015q3
+LINK=https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q3-update/+download/"${TARBALL}"
+CURDIR="$(pwd)"
+
+echo  "${MD5SUM} ${TARBALL}"  >  md5.txt
+wget  "${LINK}"
+md5sum  -c  md5.txt
+rm  md5.txt
+
+tar  xvjf  "${TARBALL}"
+rm  "${TARBALL}"
+
+if [ -n "${GITHUB_CI_PR_SHA}" ]; then
+	# This is for GitHub Actions CI/CD tooling only
+	echo  "export  PATH=\"${CURDIR}/${TARDIR}/bin\":\"\${PATH}\"" > build.env
+else
+	# And this is for local install
+	echo ""
+	echo  "Add a line like"
+	echo "export  PATH=\"${CURDIR}/${TARDIR}/bin\":\"\${PATH}\""
+	echo "to your ~/.profile and/or ~/.bashrc and run the line to enable toolchain as default one for arm-none-eabi-* target"
+	echo ""
+fi;
+
--- src/build-scripts/hydrafw-version.py
+++ src/build-scripts/hydrafw-version.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import re
 from optparse import OptionParser
 from datetime import *
 from git import *
@@ -14,7 +16,14 @@ parser = OptionParser(usage=usage)
 if len(args)==1:
   sys.stdout = open(args[0], 'w')
   git=Repo(search_parent_directories=True).git
-  print('#define HYDRAFW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"')
+  vermagic=git.describe(tags=True,always=True,dirty=True,long=True)
+  if os.environ.get("GITHUB_CI_PR_SHA", "") != "":
+    # if GITHUB_CI_PR_SHA is set, then it's GitHub Actions build, so workaround to set a valid tag is required
+    sha_id=os.environ["GITHUB_CI_PR_SHA"][:7]
+    print('#define HYDRAFW_GIT_TAG "' + re.sub('-g.*$', '-g' + sha_id, vermagic) + '"')
+  else:
+    # If it's local build, then set "normal" tag
+    print('#define HYDRAFW_GIT_TAG "' + vermagic + '"')
   print('#define HYDRAFW_CHECKIN_DATE "' + git.show(['-s', '--pretty=format:%ai']).partition(' ')[0] + '"')
 else:
   parser.print_help()
